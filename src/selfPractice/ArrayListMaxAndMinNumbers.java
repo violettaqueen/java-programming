5b56b19f1033ff276b6b50a8613784fa0be202b5
violettaqueen <violetta.v.queen@gmail.com>
2022-06-13 09:17:32 -0600

    initial commit

---
+++ src/selfPractice/ArrayListMaxAndMinNumbers.java
@@ -0,0 +1,32 @@
+package selfPractice;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ArrayListMaxAndMinNumbers {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+        list.addAll(Arrays.asList(1,2,3,4,5));
+        System.out.println(list);
+
+        int max = 4; //index number
+        int min = 0; //index number
+
+        for (Integer eachElement : list) {
+
+            if(eachElement <  max){
+                max = eachElement;
+            }
+            if(eachElement > min){
+                min = eachElement;
+            }
+        }
+        System.out.println("min = " + min);
+        System.out.println("max = " + max);
+
+    }
+
+
+}

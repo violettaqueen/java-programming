56cdf0e15b187325ec84ecca6b49b1a001b738c9
violettaqueen <violetta.v.queen@gmail.com>
2022-06-16 11:45:52 -0600

    initial commit

---
+++ src/day24_DateAndTime/FrequencyOfCharacters.java
@@ -0,0 +1,31 @@
+package day24_DateAndTime;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class FrequencyOfCharacters {
+
+    public static void main(String[] args) {
+
+        String str = "aaabbbbbbcccc";
+        // return frequency of every single character
+        // a3b5c4
+
+        String result = ""; //to contain the final result
+
+        // create a collection out if a string --> create a string array
+
+        // split the string
+        for(String each : str.split("")){
+
+            int frequency = Collections.frequency(Arrays.asList(str.split("")), each );
+
+            if(!result.contains(each)){ // to avoid a duplication, it's not going to comcate another a
+                result += each + frequency;
+            }
+
+        }
+
+    }
+
+}
--- src/mentorSessions/JadenCased.java
+++ src/mentorSessions/JadenCased.java
@@ -7,18 +7,44 @@ public class JadenCased {
     public static void main(String[] args) {
 
         String sentence = "How can mirrors be real if our eyes aren't real";
+
+        // solution 1
+
+
+    }
+
+    public String toJadenCase2(String phrase) {
+
+        if (phrase == null || phrase.isEmpty()) {
+            return "";
+        }
+        // we will store a new phrase
         String result = "";
-        String singleWord ="";
+
+        //  split the phrase by space,convert to String array
+        String[] strings = phrase.split(" ");
 
 
-
-            }
-            {
-
+        // for each loop (array)
+        for (String eachWord : strings) {
+            result += (eachWord.charAt(0) + "").toUpperCase() + eachWord.substring(1) + " ";
         }
+        return result.trim();
+    }
 
-        }
+}
 
+
+
+/*
+Your task is to convert strings to how they would be written by Jaden Smith. The
+strings are actual quotes from Jaden Smith, but they are not capitalized in the
+same way he originally typed them.
+Example:
+Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
+Jaden-Cased: "How Can Mirrors Be Real If Our Eyes Aren't Real"
+Note that this function should a return empty string for an empty string or null
+ */
 
 
 

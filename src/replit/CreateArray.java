7c93dff42c374cbd78212ac49a22dc51869b2e25
violettaqueen <violetta.v.queen@gmail.com>
2022-06-16 20:37:04 -0600

    initial commit

---
+++ src/replit/CreateArray.java
@@ -0,0 +1,16 @@
+package replit;
+
+import java.util.Arrays;
+
+public class CreateArray {
+
+    public static void main(String[] args) {
+
+        int[] array = {34, 59, 19};
+
+        System.out.println("First element: " + array[0] +"\n" + "Middle element: " + array[1] + "\n" +
+                "Last element: " + array[2]);
+
+    }
+
+}

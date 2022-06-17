0580dae3d8aeb8215cb74e5faa0cc6ceda4d254a
violettaqueen <violetta.v.queen@gmail.com>
2022-06-16 20:41:39 -0600

    initial commit

---
+++ src/replit/PrintArray.java
@@ -0,0 +1,24 @@
+package replit;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Arrays;
+
+public class PrintArray {
+    public static void main(String[] args) {
+
+        int [] num = {10, 14, -93, 140, -20};
+        System.out.println(Arrays.toString(num));
+
+    }
+
+
+
+
+
+}
+/*
+Print the given int [] nums to output all of the elements in one line.
+Note: Think about how you must print out a whole array
+Main topics: arrays, primitive data types, concatenation
+ */

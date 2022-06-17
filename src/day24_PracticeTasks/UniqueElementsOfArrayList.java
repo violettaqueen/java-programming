a6eb7b3af04aadc18d214bae925fac2bdf8bd956
violettaqueen <violetta.v.queen@gmail.com>
2022-06-17 08:54:45 -0600

    initial commit

---
+++ src/day24_PracticeTasks/UniqueElementsOfArrayList.java
@@ -0,0 +1,31 @@
+package day24_PracticeTasks;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class UniqueElementsOfArrayList {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.addAll(Arrays.asList(1, 1, 2, 3, 3, 4, 5, 5));
+        list.retainAll(Arrays.asList(2,4));
+
+
+        System.out.println(list);
+
+    }
+
+}
+/*
+Write a program that can display the unique elements of an arrayList:
+		ex:
+			list = [1, 1, 2, 3, 3, 4, 5, 5]
+
+		output:
+			[2, 4]
+
+	Note: DO NOT use any loops
+ */

3e6a1d63994f363a2a9af9d4c2ea5f9c8f4e55b0
violettaqueen <violetta.v.queen@gmail.com>
2022-06-17 11:04:19 -0600

    initial commit

--- src/day24_PracticeTasks/DateTimeFormatterIntro.java
+++ src/day24_PracticeTasks/DateTimeFormatterIntro.java
@@ -2,6 +2,7 @@ package day24_PracticeTasks;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 public class DateTimeFormatterIntro {
@@ -16,6 +17,14 @@ public class DateTimeFormatterIntro {
 
         LocalDate date1 = LocalDate.of(2022,6,12);
         System.out.println(date1.format(df)); //July-01-22
+
+        DateTimeFormatter tf = DateTimeFormatter.ofPattern("hh:mm a");
+        LocalTime time1 = LocalTime.of(7,5);
+        System.out.println(time1.format(tf));
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("EEEE, MM/dd/y  hh:mm a");
+        LocalDateTime starts = LocalDateTime.now();
+        System.out.println(starts.format(dtf));
 
 
     }

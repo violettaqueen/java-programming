e35ff6819617fec6005b1b15a00cb3699a740f94
violettaqueen <violetta.v.queen@gmail.com>
2022-06-19 12:42:35 -0600

    initial commit

---
+++ src/day26_Statics/TestCarObjects.java
@@ -0,0 +1,26 @@
+package day26_Statics;
+
+public class TestCarObjects {
+
+    public static void main(String[] args) {
+
+
+        Car car1 = new Car("BMW");
+        System.out.println(car1);
+
+        Car car2 = new Car("Audi", "Q6");
+        System.out.println(car2);
+
+        Car car3 = new Car("Porche", "Panamera", 2023);
+        System.out.println(car3);
+
+        Car car4 = new Car("Bently", "Coupe", 2024, 120000);
+        System.out.println(car4);
+
+        Car car5 = new Car("Porche", "Cayenne", 2025, 110000, "red");
+        System.out.println(car5);
+
+
+
+    }
+}

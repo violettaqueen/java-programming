3af1590bb14ae181f2a14ad2194fce90ae0d3d2f
violettaqueen <violetta.v.queen@gmail.com>
2022-06-19 15:39:15 -0600

    initial commit

--- src/day26_Statics/Employee.java
+++ src/day26_Statics/Employee.java
@@ -2,7 +2,48 @@ package day26_Statics;
 
 public class Employee {
 
+    public String name;
+    public char gender;
+    public String jobTitle;
+    public double salary;
 
+    public Employee(String name) {
+        this.name = name;
+    }
+
+    public Employee(String name, char gender) {
+        this(name);
+        this.gender = gender;
+    }
+
+    public Employee(String name, char gender, String jobTitle) {
+        this(name,gender);
+        this.jobTitle = jobTitle;
+    }
+
+    public Employee(String name, char gender, String jobTitle, double salary) {
+        this(name,gender,jobTitle);
+        this.salary = salary;
+    }
+
+    public Employee(String name, String jobTitle) {
+        this(name);
+        this.jobTitle = jobTitle;
+    }
+
+    public Employee(String name, String jobTitle, double salary) {
+        this(name,jobTitle);
+        this.salary = salary;
+    }
+
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", gender=" + gender +
+                ", jobTitle='" + jobTitle + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
 }
 /*
 Class Name: Employee

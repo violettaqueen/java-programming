88d4d8d5d6287b1ed2227589a86230ebf45ed5b2
violettaqueen <violetta.v.queen@gmail.com>
2022-06-21 11:10:42 -0600

    initial commit

---
+++ src/day26_Statics/CydeoStudent.java
@@ -0,0 +1,34 @@
+package day26_Statics;
+
+public class CydeoStudent {
+
+    public String name;
+    public int age;
+    public char gender;
+
+    public static String schoolName;
+    public static String secretCode;
+
+
+    // constructor helps to set these INSTANCE variables:
+    // don't add static variables to constructor
+
+    public CydeoStudent(String name, int age, char gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    static{
+        schoolName = "Cydeo";  // will be assigned ONE TIME ONLY (initialize) for all the objects
+        secretCode = "Wooden Spoon"; // only runs ONE time
+    }
+
+    public String toString() {
+        return "CydeoStudent {" +
+                "name ='" + name + '\'' +
+                ", age =" + age +
+                ", gender =" + gender +
+                '}';
+    }
+}

004fe754ab9d90ef2854b6275747cc0e0666c08b
violettaqueen <violetta.v.queen@gmail.com>
2022-06-21 10:48:45 -0600

    initial commit

---
+++ src/day26_Statics/StaticInitializationBlock.java
@@ -0,0 +1,21 @@
+package day26_Statics;
+
+public class StaticInitializationBlock {
+
+    public static int a;
+    public static double b;
+    public static String c;  // all object will have the same value
+
+    // public static ExcelSheet sheet; read file, get file, get a sheet from a file, etc. - 4 steps to set a variable
+    // need a block of code in order to set  a variable
+    // static block gets executed automatically, it can set any static variable
+
+    static {
+
+        a = 100;
+        b = 20.5;
+        c = "Java";
+
+    }
+    // call these variables through the class
+}

1eb258e3e39853b6c9d5ce1ae0c18151c712a751
violettaqueen <violetta.v.queen@gmail.com>
2022-06-21 10:41:03 -0600

    initial commit

---
+++ src/day26_Statics/StaticBlock.java
@@ -0,0 +1,21 @@
+package day26_Statics;
+
+public class StaticBlock {
+
+    public StaticBlock(){
+        System.out.println("Constructor"); // executes 3 times
+    }
+
+    static{
+        System.out.println("Static Block"); // executes first
+    }
+
+    public static void main(String[] args) { // main method executes second
+
+        System.out.println("Main method");
+
+        new StaticBlock(); // static block executes only one time and
+        new StaticBlock(); // constructor executes separately for each object
+        new StaticBlock();
+    }
+}

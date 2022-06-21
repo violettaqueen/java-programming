6ed76c25b95afb0489abf1f953fd447e1ec99e3a
violettaqueen <violetta.v.queen@gmail.com>
2022-06-20 23:42:48 -0600

    initial commit

--- src/selfPractice/DateTimePractice/LeapYear.java
+++ src/selfPractice/DateTimePractice/LeapYear.java
@@ -4,10 +4,20 @@ import java.time.LocalDate;
 
 public class LeapYear {
 
-    public static void main(String[] args) {
+public static boolean isLeapYear(int birthYear){
+           return LocalDate.of(birthYear, 1,1).isLeapYear();
+        }
 
     }
-}
+
+
+
+
+
+
+
+
+
 /*
 8. Create a function that accepts an int argument birthYear,
  return true if the person was born on leap year

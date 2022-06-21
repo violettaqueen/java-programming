adbb5b752218765d58fb9c377522570d69befa81
violettaqueen <violetta.v.queen@gmail.com>
2022-06-20 23:19:42 -0600

    initial commit

--- src/selfPractice/DateTimePractice/RemoveAllDates.java
+++ src/selfPractice/DateTimePractice/RemoveAllDates.java
@@ -8,7 +8,15 @@ public class RemoveAllDates {
 
     public static void main(String[] args) {
 
-        ArrayList<LocalDate> dates = new ArrayList<>();
+        ArrayList<LocalDate> dates = new ArrayList<>(Arrays.asList( LocalDate.of(2017, 1,1 )));
+
+
+
+        dates.removeIf(p -> p.isBefore(LocalDate.of(2016, 8,15)));
+
+
+        System.out.println(dates);
+
 
     }
 

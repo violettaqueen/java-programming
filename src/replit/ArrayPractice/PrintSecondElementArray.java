f72640fa264f2b629123ea141d74d580f284c2ed
violettaqueen <violetta.v.queen@gmail.com>
2022-06-22 19:18:46 -0600

    initial commit

---
+++ src/replit/ArrayPractice/PrintSecondElementArray.java
@@ -0,0 +1,12 @@
+package replit.ArrayPractice;
+
+public class PrintSecondElementArray {
+
+    public static void main(String[] args) {
+
+        int[] array = {0,1,2,3,4,5,7,8,9};
+        System.out.println("Second element: " + array[1] +"\n" + "Second to last element: " + (array.length-1));
+
+
+    }
+}

47242c321deaac26f189b7bdcbe0d8ef9275ab27
violettaqueen <violetta.v.queen@gmail.com>
2022-06-24 09:38:48 -0600

    initial commit

--- src/selfPractice/Encapsulation/TestPizza.java
+++ src/selfPractice/Encapsulation/TestPizza.java
@@ -4,13 +4,13 @@ public class TestPizza {
 
     public static void main(String[] args) {
 
-        Pizza smallPizza = new Pizza("large", 5,6);
+        Pizza pizza1  = new Pizza("large", 5,6);
 
-        smallPizza.getSize();
-        smallPizza.getNumberOfCheeseTopping();
-        smallPizza.getNumberOfPepperoniTopping();
-        smallPizza.calCost();
+        pizza1.getSize();
+        pizza1.getNumberOfCheeseTopping();
+        pizza1.getNumberOfPepperoniTopping();
+        pizza1.calCost();
 
-        System.out.println(smallPizza);
+        System.out.println(pizza1);
     }
 }

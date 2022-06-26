29d736df44b5613081ef467249c78109344be112
violettaqueen <violetta.v.queen@gmail.com>
2022-06-26 13:59:05 -0600

    initial commit

--- src/lab_Days/lab09_Encapsulation_classObject/Scrum_Task/ScrumTeam.java
+++ src/lab_Days/lab09_Encapsulation_classObject/Scrum_Task/ScrumTeam.java
@@ -1,6 +1,113 @@
 package lab_Days.lab09_Encapsulation_classObject.Scrum_Task;
 
-public class ScrumTeam {
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ScrumTeam {  //ScrumTeam class has a Tester
+
+    private String PO;
+    private String BA;
+    private String SM;
+    private ArrayList<Tester> testers = new ArrayList<>(); // we need an object
+    private ArrayList<Developer> developers = new ArrayList<>();
+    private int daysOfSprint; // every 10-15 days every Scrum team has a sprint
+
+    public String getPO() {
+        return PO;
+    }
+
+    public void setPO(String PO) {
+        terminate(PO, "Invalid PO name");
+        this.PO = PO;
+    }
+
+    public String getBA() {
+        return BA;
+    }
+
+    public void setBA(String BA) {
+        terminate(BA, "Invalid BA name");
+        this.BA = BA;
+    }
+
+    public String getSM() {
+        return SM;
+    }
+
+    public void setSM(String SM) {
+        terminate(SM, "Invalid SM name");
+        this.SM = SM;
+    }
+
+    public int getDaysOfSprint() {
+        return daysOfSprint;
+    }
+
+    public void setDaysOfSprint(int daysOfSprint) {
+
+        if (daysOfSprint <= 0) {
+            System.err.println("Invalid days of Sprint: " + daysOfSprint);
+            System.exit(1);
+        }
+        this.daysOfSprint = daysOfSprint;
+    }
+
+    public ArrayList<Tester> getTesters() {
+        return testers;
+    }
+
+    public ArrayList<Developer> getDevelopers() {
+        return developers;
+    }
+
+    private static void terminate(String arg, String err) {
+
+        if (arg == null || arg.trim().isEmpty()) {
+            System.err.println(err);
+            System.exit(1);
+        }
+    }
+
+    public ScrumTeam(String PO, String BA, String SM, int daysOfSprint) {
+        setPO(PO);
+        setBA(BA);
+        setSM(SM);
+        setDaysOfSprint(daysOfSprint);
+    }
+
+    public void addTester(Tester tester) {
+        testers.add(tester);
+    }
+
+    public void addTester(Tester[] testers) {
+        this.testers.addAll(Arrays.asList(testers));
+    }
+
+    public void addDeveloper(Developer developer) {
+        developers.add(developer);
+    }
+
+    public void addDeveloper(Developer[] developers) {
+        this.developers.addAll(Arrays.asList(developers));
+    }
+
+    public void removeTester(String id) {
+        testers.removeIf(p -> p.getEmployeeID().equals(id));
+    }
+
+    public void removeDeveloper(String id) {
+        developers.removeIf(p -> p.getEmployeeID().equals(id));
+    }
+
+    public String toString() {
+        return "ScrumTeam{" +
+                "PO='" + PO + '\'' +
+                ", BA='" + BA + '\'' +
+                ", SM='" + SM + '\'' +
+                ", testers=" + testers.size() + // to print how many testers
+                ", developers=" + developers.size() +
+                '}';
+    }
 }
 /*
 create a class called ScrumTeam

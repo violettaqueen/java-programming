d1e240f1fcf79d1e3e87eb8ec9f8e4ba2ef85ec7
violettaqueen <violetta.v.queen@gmail.com>
2022-06-28 14:45:05 -0600

    initial commit

--- src/selfPractice/String/DigitLetterSpecialChar.java
+++ src/selfPractice/String/DigitLetterSpecialChar.java
@@ -11,21 +11,33 @@ public class DigitLetterSpecialChar {
         System.out.println("Please enter a word:");
         String word = input.next();
 
-        char firstDigit = word.charAt(0)
-
-        if (word.charAt(0) ==  ){
-            System.out.println("First character id digit");
-        } else if (word.charAt(0)) {
-            System.out.println("first character is lowercase letter");
-        } else if () {
-            System.out.println("first character is uppercase letter");
-        }else{
-            System.out.println("first character is special character");
-        }
         input.close();
 
+        // 1. Should check a length of a string in order to get a first character because what if it's an empty string
+        // 2. 4 conditions to create:
+
+        if (word.length() >= 1) {
+            char first = word.charAt(0);
+
+            if (first >= 48 && first <= 57) {  // first >= '0' && first <= '9'
+                System.out.println("First character id digit");
+            } else if (first >= 'A' && first <= 'Z') {
+                System.out.println("first character is uppercase letter");
+            } else if (first >= 'a' && first <= 'z') {
+                System.out.println("first character is lowercase letter");
+            } else {
+                System.out.println("first character is special character");
+            }
+
+            }else {
+                System.out.println("Word is empty");
+            }
+
+
+
+        }
     }
-}
+
 /*
  Create a class named DigitLetterSpecialChar and wirte a program that can ask the user to enter a word
             - if the word starts with digits, print "first character is digit"

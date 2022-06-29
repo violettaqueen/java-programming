18f830bcdf4baf610e4c304850c4e38900980dde
violettaqueen <violetta.v.queen@gmail.com>
2022-06-29 14:36:51 -0600

    initial commit

---
+++ src/selfPractice/String/Replace.java
@@ -0,0 +1,27 @@
+package selfPractice.String;
+
+import java.util.Scanner;
+
+public class Replace {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a word:");
+        String word = input.next();
+
+        if (word.startsWith("x")){
+            word = word.replaceFirst("x", "a");
+        }
+
+        System.out.println(word);
+    }
+}
+/*
+2. Write a program that asks user to enter a word. If the work starts with x, replace it with a.
+	                    Input:
+	                        xcodex
+	                    Output:
+	                        acodex
+ */

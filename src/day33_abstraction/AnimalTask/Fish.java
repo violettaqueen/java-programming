eeabe50c9ee4a2eac0195e011fa95e3c7e8b7aa0
violettaqueen <violetta.v.queen@gmail.com>
2022-07-03 16:19:43 -0600

    initial commit

--- src/day33_abstraction/AnimalTask/Fish.java
+++ src/day33_abstraction/AnimalTask/Fish.java
@@ -1,4 +1,20 @@
 package day33_abstraction.AnimalTask;
 
-public class Fish {
+import java.time.LocalDate;
+
+public class Fish extends Animals{
+
+    public Fish(String name, String breed, char gender, String size, String color, LocalDate dateOfbirth) {
+        super(name, breed, gender, size, color, dateOfbirth);
+    }
+
+    @Override
+    public void eat() {
+        System.out.println(getName()+" is eating fish food");
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println(getName()+" is sleeping");
+    }
 }

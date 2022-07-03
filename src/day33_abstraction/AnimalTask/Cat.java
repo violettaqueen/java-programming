902d7fd2b1c84d072a97c61bc5aadf3c03e42805
violettaqueen <violetta.v.queen@gmail.com>
2022-07-03 16:18:06 -0600

    initial commit

--- src/day33_abstraction/AnimalTask/Cat.java
+++ src/day33_abstraction/AnimalTask/Cat.java
@@ -1,4 +1,23 @@
 package day33_abstraction.AnimalTask;
 
-public class Cat {
+import java.time.LocalDate;
+
+public class Cat extends Animals{
+
+    public Cat(String name, String breed, char gender, String size, String color, LocalDate dateOfbirth) {
+        super(name, breed, gender, size, color, dateOfbirth);
+    }
+
+    @Override
+    public void eat() {
+        System.out.println(getName()+" is eating cat food");
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println(getName() + " is sleeping");
+    }
+    public void meow(){
+        System.out.println(getName()+ " is meowing");
+    }
 }

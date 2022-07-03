fc89e893c9eb0547e4b7308c5d83b08570b1c8e8
violettaqueen <violetta.v.queen@gmail.com>
2022-07-03 13:28:54 -0600

    initial commit

---
+++ src/day33_abstraction/EmployeeTask/Teacher.java
@@ -0,0 +1,12 @@
+package day33_abstraction.EmployeeTask;
+
+public class Teacher extends Employee{
+
+    public Teacher(String name, int age, char gender, String id, String jobTitle, double salary) {
+        super(name, age, gender, id, jobTitle, salary);
+    }
+    @Override
+    public void work() {
+        System.out.println(getName() + " is teaching");
+    }
+}
---
+++ src/day33_abstraction/EmployeeTask/Tester.java
@@ -0,0 +1,13 @@
+package day33_abstraction.EmployeeTask;
+
+public class Tester extends Employee{
+
+    public Tester(String name, int age, char gender, String id, String jobTitle, double salary) {
+        super(name, age, gender, id, jobTitle, salary);
+    }
+
+    @Override
+    public void work() {
+        System.out.println(getName()+" is testing the application");
+    }
+}

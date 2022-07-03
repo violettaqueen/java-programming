e8aef42ef67ee16703fb3bb8b73c4f2885bbffee
violettaqueen <violetta.v.queen@gmail.com>
2022-07-03 15:20:19 -0600

    initial commit

--- src/day14_forLoop/ForLoopIntro.java
+++ src/day14_forLoop/ForLoopIntro.java
@@ -5,9 +5,22 @@ public class ForLoopIntro {  //use for Loop if you know exactly how many times t
     public static void main(String[] args) {
 
 
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+
+        System.out.println("----------------------------------------------");
+
+
                  //boolean condition
-        for(int i = 1; i <= 10; i++){   //i: 1, 2
-            System.out.println("Hello world");
+        for(int i = 1; i <= 10; i++){   //i: 1, 2, 3,4,5,6,7,8,9,10
+            System.out.println("Hello world"); //1,2,3,4,5,6,7,8,9,10
         }
 
 

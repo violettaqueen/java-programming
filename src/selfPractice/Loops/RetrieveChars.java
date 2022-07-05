64d09bd7587a747a20dd4323645615d8de45343a
violettaqueen <violetta.v.queen@gmail.com>
2022-07-05 11:20:46 -0600

    initial commit

--- src/selfPractice/Loops/RetrieveChars.java
+++ src/selfPractice/Loops/RetrieveChars.java
@@ -1,25 +1,35 @@
 package selfPractice.Loops;
 
+import utilities.StringUtility;
+
 public class RetrieveChars {
 
     public static void main(String[] args) {
 
         String str = "mn@#123Ab!";
-        String result = "";
+
+        String isDigit ="";
+        String isLetter = "";
+        String isSpecialChar = "";
 
         for (int i = 0; i <= str.length()-1; i++) {
 
             char each = str.charAt(i);
 
-            if (each >= 48 && each <= 57){
-                result += " " + each;
+            if (each >= '0' && each <= '9'){
+                isDigit +=  each;
             } else if (each >= 'a' && each <= 'z' || each>='A' && each <='Z') {
-                result += each;
+               isLetter += each;
             }else{
-                result += each;
+                isSpecialChar += each;
             }
         }
-        System.out.println(result);
+        System.out.println("letters: " + isLetter);
+        System.out.println("digits: " + isDigit);
+        System.out.println("special characters: " + isSpecialChar);
+
+
+        System.out.println(StringUtility.digitLetterSpecChar("Violett123!@#"));
 
     }
 }
--- src/utilities/StringUtility.java
+++ src/utilities/StringUtility.java
@@ -12,11 +12,34 @@ public class StringUtility {
         return reverse;
     }
 
-    public static void initials(String firstName, String lastName) {
+    public static String initials(String firstName, String lastName) {
 
         firstName = firstName.substring(0, 1).toUpperCase();
         lastName = lastName.substring(0, 1).toUpperCase();
-        System.out.println(firstName + "." + lastName + ".");
+        return (firstName + "." + lastName + ".");
+    }
+    public static String digitLetterSpecChar(String str){
+
+        String isDigit ="";
+        String isLetter = "";
+        String isSpecialChar = "";
+
+        for (int i = 0; i <= str.length()-1; i++) {
+
+            char each = str.charAt(i);
+
+            if (each >= '0' && each <= '9'){
+                isDigit +=  each;
+            } else if (each >= 'a' && each <= 'z' || each>='A' && each <='Z') {
+                isLetter += each;
+            }else{
+                isSpecialChar += each;
+            }
+        }
+        return ("letters: " + isLetter + "\n" +"digits: " + isDigit +"\n"+ "special characters: " + isSpecialChar);
+
+
+
     }
 
 }

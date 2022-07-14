0cd28fdae941c5960478dcc98ad5e46274c740bc
violettaqueen <violetta.v.queen@gmail.com>
2022-07-14 14:38:58 -0600

    initial commit

--- src/selfPractice/Loops/day15_whileLoop/DivisibleBy.java
+++ src/selfPractice/Loops/day15_whileLoop/DivisibleBy.java
@@ -6,19 +6,26 @@ public class DivisibleBy {
 
         int num = 100;
 
-        int divisibleBy15 = 0;
-        int divisibleBy3 = 0;
-        int divisibleBy5 = 0;
-        for (int i = 1; i <= 100; i++) {
+        String divisibleBy15 = "";
+        String divisibleBy3 = "";
+        String divisibleBy5 = "";
+
+        for (int i = 1; i <= num; i++) {
 
 
-            if (i % 15 == 0 && i % 5 == 0 && i % 3 == 0) {
-                divisibleBy15 += i;
-            }  {
-
+            if (i % 15 == 0 &&  i % 3 == 0) {
+                divisibleBy15 += i + " ";
+            }
+            if(i %5 == 0 && i %15 !=0){
+                divisibleBy5 += i + " ";
+            }
+            if (i %3 == 0 && i% 15 !=0){
+                divisibleBy3 += i + " ";
             }
         }
-        System.out.println(divisibleBy15);
+        System.out.println("Divisible by 15: " +divisibleBy15);
+        System.out.println("Divisible by 5: " +divisibleBy5);
+        System.out.println("Divisible by 3: " +divisibleBy3);
     }
 }
 /*

1496162c4b15e58812d9d384d194c0b41b37912d
violettaqueen <violetta.v.queen@gmail.com>
2022-07-15 13:46:57 -0600

    initial commit

--- src/day39_collections/ListDemo.java
+++ src/day39_collections/ListDemo.java
@@ -1,6 +1,36 @@
 package day39_collections;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class ListDemo {
 
+    public static void main(String[] args) {
+
+        ArrayList<Integer> arrayList = new ArrayList<>(); // Array Based Class ===> get() is faster
+        arrayList.add(100);
+        arrayList.add(200);
+        arrayList.add(300);
+
+        arrayList.remove(0);
+
+        arrayList.get(0);
+
+        LinkedList<Integer> linkedList = new LinkedList<>(); // Node based class (Doubly linked List) ===> add(), addAll(), remove(), removeAll(), removeIf(), retainAll() are faster
+
+        linkedList.add(100);
+        linkedList.add(200);
+        linkedList.add(300);
+
+        linkedList.remove(0);
+
+        linkedList.get(0);
+
+
+        System.out.println("------------------------------------------------------------");
+
+
+
+    }
 
 }

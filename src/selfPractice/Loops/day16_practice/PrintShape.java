dd7be1c9a04c4290deff91f525ea88ef02b1c576
violettaqueen <violetta.v.queen@gmail.com>
2022-07-15 12:41:52 -0600

    initial commit

---
+++ src/selfPractice/Loops/day16_practice/PrintShape.java
@@ -0,0 +1,35 @@
+package selfPractice.Loops.day16_practice;
+
+public class PrintShape {
+
+    public static void main(String[] args) {
+
+
+        for (int j = 1; j <=10 ; j++) {
+
+
+            for (int i = 0; i < j; i++) {
+            System.out.print("* ");
+        }
+            System.out.println();
+        }
+
+
+
+
+    }
+}
+/*
+Use a nested loop to print the following shape
+
+        *
+        * *
+        * * *
+        * * * *
+        * * * * *
+        * * * * * *
+        * * * * * * *
+        * * * * * * * *
+        * * * * * * * * *
+        * * * * * * * * * *
+ */

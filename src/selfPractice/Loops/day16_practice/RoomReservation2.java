c6aac92aaf60c5cef9f5caf490d465e620981c07
violettaqueen <violetta.v.queen@gmail.com>
2022-07-18 16:38:06 -0600

    initial commit

--- src/selfPractice/Loops/day16_practice/RoomReservation2.java
+++ src/selfPractice/Loops/day16_practice/RoomReservation2.java
@@ -9,10 +9,42 @@ public class RoomReservation2 {
         Scanner input = new Scanner(System.in);
         int total = 0;
 
+        while(true) {
 
+            System.out.println("Which room would you like to select?");
+            System.out.println("\t\tKing Bed == $120");
+            System.out.println("\t\tQueen Bed == $100");
+            System.out.println("\t\tSingle Bed == $80");
 
+            String word = input.nextLine().toLowerCase();
+
+            while (!(word.equals("king bed") || word.equals("queen bed") || word.equals("single bed"))) {
+                System.out.println("Invalid Entry, Please Re-Enter");
+                word = input.nextLine().toLowerCase();
+            } // to make sure user selected either the king bed, or queen bed or single bed
+
+            total += (word.equals("king bed")) ? 120 : (word.equals("queen bed")) ? 100 : 80;
+
+            System.out.println("Would you like to select another room?");
+            String a = input.nextLine().toLowerCase();
+
+            while( !( a.equals("yes") || a.equals("no") ) ){ // while the answer is not valid answer
+                System.out.println("Invalid Entry, please re-enter");
+                System.out.println("Would you like to select another room?");
+                a = input.nextLine().toLowerCase();
+            } // it stops if the answer is either yes or no
+
+            if(a.equals("no")){
+                System.out.println("Your total price is: $"+total);
+                break;
+            }
+
+        }
+
+        input.close();
 
     }
+
 }
 /*
 Create a class called RoomReservation, write a program for the room reservation:
@@ -25,3 +57,7 @@ Create a class called RoomReservation, write a program for the room reservation:
                             if no ==> return the  total price
                 If user enters any invalid entry,  ask the user to re-enter until user provides a valid entry
  */
+
+
+
+

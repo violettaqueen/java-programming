73934e9516ba87d711f50e90d4dfe4ebc518fe1d
violettaqueen <violetta.v.queen@gmail.com>
2022-07-22 11:53:31 -0600

    initial commit

--- src/replit/ArrayPractice/ReverseSentence.java
+++ src/replit/ArrayPractice/ReverseSentence.java
@@ -7,15 +7,15 @@ public class ReverseSentence {
     public static void main(String[] args) {
 
         String sentence = "Java is fun";
-        String reverse = "";
 
-        String[] sentenceArray = {"Java is fun"};
+        String[] sentenceArray = {"Java is fun"}; // convert String object to an array object
 
-        sentenceArray = sentence.split(" ");
+        sentenceArray = sentence.split(" "); // split array into an array of Strings
 
-        for (int i = sentenceArray.length; i > 0; i--) {
-
+        for (int i = sentenceArray.length-1; i >= 0; i--) { // iterate in reverse order
+            System.out.print(sentenceArray[i].trim() + " ");
         }
+
 
     }
 }

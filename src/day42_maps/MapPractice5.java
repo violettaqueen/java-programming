2501ec15e502dc80e7a6eab79fa463a70af99bd4
violettaqueen <violetta.v.queen@gmail.com>
2022-07-21 20:50:07 -0600

    initial commit

---
+++ src/day42_maps/MapPractice5.java
@@ -0,0 +1,22 @@
+package day42_maps;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class MapPractice5 {
+
+    public static void main(String[] args) {
+
+        Map<String, String> map = new TreeMap<>();
+        map.put("C02", "Josh");
+        map.put("B03", "Nora");
+        map.put("A02", "Khashayar");
+        map.put("D02", "Abidullah");
+        map.put("A03", "Tatiana");
+
+        // iterate forEach method:
+
+        map.forEach( (k, v) -> { System.out.println(k +" : "+ v); } );
+
+    }
+}

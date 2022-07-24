6d60ee5d5d6077fc5fcd29d43c29dd55fc989586
violettaqueen <violetta.v.queen@gmail.com>
2022-07-23 21:53:46 -0600

    initial commit

--- src/replit/ArrayPractice/Pairs.java
+++ src/replit/ArrayPractice/Pairs.java
@@ -7,15 +7,17 @@ public class Pairs {
         String[] pairs = {"apple", "banana", "kiwi", "grape", "milk", "soda", "juice", "coffee"};
 
         for (int i = 0; i < pairs.length; i++) {
-
+            System.out.println(pairs[i] +" " + pairs[i+1]);
 
             }
         }
     }
 
 /*
-Given a String [] arr, print a pair of items together in one line until all the pairs are printed. A pair is a element and the element next to it. Print each item with a comma in between them.
-Note: The loop used in the given code is to dynamically create the String array. You don't need to fully understand this part yet, focus only on having access to an array.
+Given a String [] arr, print a pair of items together in one line until all the pairs are printed. A pair is a element and the
+element next to it. Print each item with a comma in between them.
+Note: The loop used in the given code is to dynamically create the String array. You don't need to fully understand this part yet,
+ focus only on having access to an array.
 Main topics: arrays, primitive data types, concatenation, index, operators, loops, String
 
 Example:
--- src/replit/loops/UtopianTree.java
+++ src/replit/loops/UtopianTree.java
@@ -8,19 +8,52 @@ public class UtopianTree {
         int growth = 0;
         int treeSize = 0;
 
-        for (int i = 1; i <= 10 ; i++) {
+        for (int i = 0, k = 1; year <= 10; i++, k++) {
             year = i;
             for (int j = 0; j < i; j++) {
                 growth = j;
-
-
-
+                if (i == 1) {
+                    growth++;
+                    treeSize+= i;
                 }
+
+
             }
         }
-
-
-
+        System.out.println(treeSize);
     }
 
 
+}
+
+/*
+Use a loop to track the growth of the Utopian Tree. The tree grows exactly 1 cm for the first three years, and then
+ afterwards it grows by 2 cm every year. Show the growth of the tree each year up to year 10. See the example flow below.
+Use the format:
+year x - growth x cm
+tree size: x cm
+
+where the 'x' values are different values of year, the growth amount, and the total tree size up to that year
+Main topics: loops, primitive datatypes, operators, concatenation, if statements
+
+Example Flow:
+
+year 1 - growth 1 cm
+tree size: 1 cm
+
+year 2 - growth 1 cm
+tree size: 2 cm
+year 3  - growth 1 cm
+tree size: 3 cm
+
+year 4 - growth 2 cm
+tree size: 5 cm
+
+year 5 - growth 2 cm
+tree size: 7 cm
+
+year 6 - growth 2 cm
+tree size: 9 cm
+
+... until you reach year 10
+ */

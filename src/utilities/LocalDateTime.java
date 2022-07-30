f821fcc92ac9e25d75e0a15dee1f873e28f27884
violettaqueen <violetta.v.queen@gmail.com>
2022-07-30 12:13:17 -0600

    initial commit

--- src/utilities/LocalDateTime.java
+++ src/utilities/LocalDateTime.java
@@ -1,12 +1,18 @@
 package utilities;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class LocalDateTime {
 
     public static boolean bornOnLeapYear(int birthYear){
 
         return  LocalDate.of(birthYear,1,1).isLeapYear();
+    }
+    public static ArrayList<LocalDate> removeDates(ArrayList<LocalDate> list){
+
+        list.removeIf(p -> p.isBefore(LocalDate.of(2016,8,15)));
+        return list;
     }
 }
 

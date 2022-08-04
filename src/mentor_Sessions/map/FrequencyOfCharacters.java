297bff1ec80e1a6e99ae71bc1b3a6ac0e1715253
violettaqueen <violetta.v.queen@gmail.com>
2022-08-04 16:10:03 -0600

    initial commit

--- src/mentor_Sessions/map/FrequencyOfCharacters.java
+++ src/mentor_Sessions/map/FrequencyOfCharacters.java
@@ -1,4 +1,33 @@
 package mentor_Sessions.map;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class FrequencyOfCharacters {
+
+    public static void main(String[] args) {
+
+        String str = null;
+
+        Map<Character, Integer> map = new LinkedHashMap<>();
+
+        for (String each : str.split("")) {
+            int frequency = Collections.frequency(Arrays.asList(str.split("")), each);
+            if (str.isEmpty()) {
+                map.clear();
+            } else if (str == null) {
+                System.out.println("null");
+            } else {
+                map.put(each.charAt(0), frequency);
+
+            }
+
+
+
+
+        }
+        System.out.println(map);
+    }
 }
